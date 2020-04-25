b5e30816e895dfa485e4e066b79f3fe09b1a8679
Ike Maldonado <maldonadoike@gmail.com>
2020-04-25 02:45:30 +0000

    Initial Commit

---
+++ Posts.js
@@ -0,0 +1,16 @@
+const posts = [
+	{
+		id: 1,
+		name: 'Ikey',
+		postmessage: 'Post your comments, please!',
+		status: 'active'
+	},
+	{
+		id: 2,
+		name: 'ExampleName',
+		postmessage: 'Example comment',
+		status: 'active'
+	}
+];
+
+module.exports = posts;
---
+++ routes/api/posts.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const uuid = require('uuid');
+const router = express.Router();
+const posts = require('../../Posts');
+
+// Gets All Posts
+router.get('/', (req, res) => {
+	res.json(posts);
+});
+
+// Gets Single Post
+router.get('/:id', (req, res) => {
+	const found = posts.some((post) => post.id === parseInt(req.params.id));
+
+	if (found) {
+		res.json(posts.filter((post) => post.id === parseInt(req.params.id)));
+	} else {
+		res.status(400).json({ msg: `No post with the id of ${req.params.id}` });
+	}
+});
+
+// Create Post
+router.post('/', (req, res) => {
+	const newPost = {
+		id: uuid.v4(),
+		name: req.body.name,
+		postmessage: req.body.postmessage,
+		status: 'active'
+	};
+
+	if (!newPost.name || !newPost.postmessage) {
+		return res.status(400).json({ msg: 'Please include a name and post message' });
+	}
+
+	posts.push(newPost);
+	res.redirect('/');
+});
+
+// Update Post
+router.put('/:id', (req, res) => {
+	const found = posts.some((post) => post.id === parseInt(req.params.id));
+
+	if (found) {
+		const updPost = req.body;
+		posts.forEach((post) => {
+			if (post.id === parseInt(req.params.id)) {
+				post.name = updPost.name ? updPost.name : post.name;
+				post.name = updPost.postmessage ? updPost.postmessage : post.postmessage;
+
+				res.json({ msg: 'Post updated', post });
+			}
+		});
+	} else {
+		res.status(400).json({ msg: `No post with the id of ${req.params.id}` });
+	}
+});
+
+// Delete Post
+router.delete('/:id', (req, res) => {
+	const found = posts.some((post) => post.id === parseInt(req.params.id));
+
+	if (found) {
+		res.json({ msg: 'Post deleted', posts: posts.filter((post) => post.id !== parseInt(req.params.id)) });
+	} else {
+		res.status(400).json({ msg: `No post with the id of ${req.params.id}` });
+	}
+});
+
+module.exports = router;
